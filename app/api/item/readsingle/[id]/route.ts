91f8d94832f2e4b6cda9cb8685c758c6533be723
nsznsnb <nsznsnb@gmail.com>
2024-09-29 07:18:40 +0900

    使用するprismaオブジェクトの変更

--- app/api/item/readsingle/[id]/route.ts
+++ app/api/item/readsingle/[id]/route.ts
@@ -1,4 +1,5 @@
 import { NextRequest, NextResponse } from "next/server";
+import prisma from "@/app/lib/prisma";
 
 export async function GET(
   request: NextRequest,
@@ -7,7 +8,7 @@ export async function GET(
   console.log(context.params.id);
   const id = context.params.id;
   try {
-    const singleItem = await prisma?.item.findUnique({
+    const singleItem = await prisma.item.findUnique({
       where: {
         id: id,
       },
